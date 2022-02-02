c3d579693532588516f29e4da8e277177c2a5f50
vvitoss <orangekaramel@gmail.com>
2022-02-02 15:45:15 +0300

    initial commit

---
+++ src/main/java/ru/netology/api/FormDate.java
@@ -0,0 +1,7 @@
+package ru.netology.api;
+
+public class FormDate {
+    public String day;
+    public String month;
+    public String year;
+}
---
+++ src/main/java/ru/netology/api/Post.java
@@ -0,0 +1,15 @@
+package ru.netology.api;
+
+public class Post {
+    public FormDate birthday;
+    public String name;
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public boolean subscription;
+
+
+
+
+}
