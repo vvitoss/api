7e83e139efcb0770f477bdb4f2a188f0b6836faa
vvitoss <orangekaramel@gmail.com>
2022-02-05 20:51:02 +0300

    fixed variables in faile formdata

--- src/main/java/ru/netology/api/FormDate.java
+++ src/main/java/ru/netology/api/FormDate.java
@@ -1,7 +1,7 @@
 package ru.netology.api;
 
 public class FormDate {
-    public String day;
-    public String month;
-    public String year;
+    public int day;
+    public int month;
+    public int year;
 }
--- src/main/java/ru/netology/api/Main.java
+++ src/main/java/ru/netology/api/Main.java
@@ -4,9 +4,9 @@ public class Main {
     public static void main(String[] args) {
         Post post = new Post();
         post.birthday = new FormDate();
-        post.birthday.day = "13";
-        post.birthday.month = "6";
-        post.birthday.year = "1999";
+        post.birthday.day = 13;
+        post.birthday.month = 6;
+        post.birthday.year = 1999;
         post.name = "Иван";
         post.passport = "4444 № 44444444";
         post.patronymic = "Иванович";
